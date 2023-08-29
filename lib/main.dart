0064a51d41f2fb3db7eaab22a2c9fc2aafe9115a
Yashwanth <yashwanth051202@gmail.com>
2023-08-29 10:55:22 +0530

    Fitst commit

---
+++ lib/main.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: AuthPage(),
+    );
+  }
+}
+
+class AuthPage extends StatefulWidget {
+  final Color? primaryColor;
+  final Color? backgroundColor;
+
+  AuthPage({
+    Key? key,
+    this.primaryColor = const Color(0xFFFFFFFF),
+    // this.backgroundColor = const Color.fromARGB(255, 27, 67, 77),
+    this.backgroundColor = const Color.fromARGB(255, 249, 243, 243),
+  });
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final myController1 = TextEditingController();
+  final myController2 = TextEditingController();
+  @override
+  void dispose() {
+    myController1.dispose();
+    myController2.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var _messangerKey;
+    var hi = MediaQuery.of(context).size.height;
+    var wi = MediaQuery.of(context).size.width;
+    return MaterialApp(
+      scaffoldMessengerKey: _messangerKey,
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: this.widget.backgroundColor,
+            ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Expanded(
+                    child: ClipPath(
+                      clipper: MyClipper(),
+                      child: Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.only(top: 100.0, bottom: 100.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              "Funwitcode",
+                              style: TextStyle(
+                                  fontFamily: 'sans-serif',
+                                  fontSize: 40.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                            Text(
+                              "MCE Hassan",
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  //everything goes here
+                  Container(
+                    padding:
+                        EdgeInsets.fromLTRB(wi * 0.1, 10, wi * 0.1, hi * 0.08),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          margin: const EdgeInsets.all(5),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Navigator.pushReplacement(
+                              //   context,
+                              //   MaterialPageRoute(
+                              //     builder: (context) => AuthFormFaculty(),
+                              //   ),
+                              // );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 27, 67, 77),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(25.0),
+                                  topRight: Radius.circular(25.0),
+                                ),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0,
+                              ),
+                              textStyle: const TextStyle(fontSize: 20.0),
+                            ),
+                            child: const Text('ADMIN'),
+                          ),
+                        ),
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          margin: const EdgeInsets.all(5),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Navigator.pushReplacement(
+                              //   context,
+                              //   MaterialPageRoute(
+                              //       builder: (context) => AuthFormStudent()),
+                              // );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 27, 67, 77),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(25.0),
+                                  bottomRight: Radius.circular(25.0),
+                                ),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16.0,
+                              ),
+                              textStyle: const TextStyle(fontSize: 20.0),
+                            ),
+                            child: const Text(
+                              'STUDENT',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path p = Path();
+    p.lineTo(size.width, 0.0);
+    p.lineTo(size.width, size.height * 0.85);
+    p.arcToPoint(
+      Offset(0.0, size.height * 0.85),
+      radius: const Radius.elliptical(50.0, 10.0),
+      rotation: 0.0,
+    );
+    p.lineTo(0.0, 0.0);
+    p.close();
+    return p;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return true;
+  }
+}
